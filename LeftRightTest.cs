c061e144a89bc9e3effb6c060131839e68f80b92
Kureyş Alp Kılıç <50348497+kureysalp@users.noreply.github.com>
2021-04-17 13:55:36 +0300

    Create LeftRightTest.cs

---
+++ LeftRightTest.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class LeftRightTest : MonoBehaviour 
+{
+  float AngleDir(Vector3 fwd, Vector3 targetDir, Vector3 up)
+    {
+        Vector3 perp = Vector3.Cross(fwd, targetDir);
+        float dir = Vector3.Dot(perp, up);
+
+        if (dir > 0f)
+        {
+            return 1f;
+        }
+        else if (dir < 0f)
+        {
+            return -1f;
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+}
